a825ddaa77a1b5e5a6887610fd7d808425bb3322
Dhairya Chheda <dhairyakchheda@gmail.com>
2023-02-20 20:05:47 +0530

    Configured routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./components/pages/Explore";
+import Offers from "./components/pages/Offers";
+import SignIn from "./components/pages/SignIn";
+import SignUp from "./components/pages/SignUp";
+import ForgotPassword from "./components/pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
     </>
   );
 }
